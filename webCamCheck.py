21055d0ef29cdbad97da8376532ac9ee38045b9b
Shleime <maxter161@gmail.com>
2013-11-06 21:00:01 +0200

    Create webCamCheck.py
    simple function to check if webcam is working using opencv

---
+++ webCamCheck.py
@@ -0,0 +1,19 @@
+import cv2
+
+def checkWebcam(camNum):
+  while True:
+		ret, frame = capt.read()
+		if ret:
+			cv2.imshow('webcam', frame)
+			k = cv2.waitKey(1)
+			if k == 27:
+				break
+	cv2.destroyAllWindows()
+	
+	
+def main():
+  checkWebcam(0)
+  
+  
+if __name__ == "__main__":
+  main()
