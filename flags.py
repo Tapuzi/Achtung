d40c58da0f671702a647451e3ac3e63d56afd025
hjbyt <sopu00@gmail.com>
2013-11-09 14:37:33 +0200

    Changed the flags module to allow debugger specific flags

--- flags.py
+++ flags.py
@@ -1,17 +1,51 @@
 
 
+# Uncomment your's when you are working
+DEBUGGER = 'hjbyt'
+##DEBUGGER = 'X-reX'
+##DEBUGGER = 'Shleime'
+##DEBUGGER = 'papaya91'
+##DEBUGGER = 'alon7'
+
+#
+# Defaults
+#
+
 DEBUG = True
-
-DEBUG_WEBCAM = False
+DEBUG_WEBCAM = True
 DEUBG_WEBCAM_WITH_WINDOW = False
-
-# Set to false to debug with watch controllers
-DEBUG_KEYBOARD = True
-
+DEBUG_KEYBOARD = False
 DEBUG_KEYBOARD_TWO_PLAYERS = False
-
 DEBUG_MOUSE = False
+FULLSCREEN = True
+
+#
+# Debugger specific
+#
+
+try:
+    if DEBUGGER == 'hjbyt':
+        DEBUG = True
+        DEBUG_WEBCAM = False
+        DEUBG_WEBCAM_WITH_WINDOW = False
+        DEBUG_KEYBOARD = True
+        DEBUG_KEYBOARD_TWO_PLAYERS = False
+        DEBUG_MOUSE = False
+        FULLSCREEN = False
+    elif DEBUGGER == 'X-reX':
+        pass
+    elif DEBUGGER == 'Shleime':
+        pass
+    elif DEBUGGER == 'papaya91':
+        pass
+    elif DEBUGGER == 'alon7':
+        pass
+except NameError:
+    # DEBUGGER not defined
+    pass
+
+#
+# Derivatives
+#
 
 DEBUG_SINGLE_PLAYER = DEBUG and not DEBUG_KEYBOARD_TWO_PLAYERS
-
-FULLSCREEN = False
